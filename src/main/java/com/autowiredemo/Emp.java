3b5489827db4a97fa4ead27b8f54b55738e1491c
Atharva Kumar <iamatharvakumar@gmail.com>
2025-05-26 19:13:24 +0530

    Understanding Autowirirng.Earlier IOC container would generate object and we would inject into the class. Now we are autowiring so that the injection happens automatically.

---
+++ src/main/java/com/autowiredemo/Emp.java
@@ -0,0 +1,21 @@
+package com.autowiredemo;
+
+public class Emp {
+
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "address=" + address +
+                '}';
+    }
+}
